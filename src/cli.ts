3b3ea617b42ce12f9006a916448e5a880362a3cc
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2022-04-01 21:31:07 +0300

    Add node-fetch.

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 
+import fetch from "node-fetch";
+import type { Response } from "node-fetch";
+
 import { darkGray, green, red, reset, yellow } from "./colors.js";
 
 const version = "0.1.0";
@@ -15,6 +18,13 @@ const args = process.argv.slice(2);
 function getUniqueCharacters(input: string) {
   return Array.from(new Set(input)).sort();
 }
+
+const checkStatus = (response: Response) => {
+  // response.ok = (response.status >= 200 && response.status < 300)
+  if (response.ok) return response;
+
+  throw new Error(response.statusText);
+};
 
 function printHelp() {
   console.log(`Usage: ${programName} [OPTION].. <URL>
@@ -35,59 +45,72 @@ For bugs and feature requests, please open an issue at https://github.com/your_u
 `);
 }
 
-if (args.includes("--help") || args.includes("-h")) {
-  printHelp();
-  process.exit(0);
-} else if (args.includes("--version") || args.includes("-v")) {
-  console.log(version);
-  process.exit(0);
-} else {
-  const positional = args.filter((_argument) => !_argument.startsWith("-"));
-  if (positional.length === 0) {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async function main() {
+  if (args.includes("--help") || args.includes("-h")) {
     printHelp();
-    console.error(
-      `${red}error${reset} (${programName}): Please provide a URL.`
-    );
-    process.exit(1);
+    process.exit(0);
+  } else if (args.includes("--version") || args.includes("-v")) {
+    console.log(version);
+    process.exit(0);
+  } else {
+    const positional = args.filter((_argument) => !_argument.startsWith("-"));
+    if (positional.length === 0) {
+      printHelp();
+      console.error(
+        `${red}error${reset} (${programName}): Please provide a URL.`
+      );
+      process.exit(1);
+    }
+
+    if (positional.length > 1) {
+      console.warn(
+        `${yellow}warning${reset} (${programName}): You provided ${positional.length} URLs, but the program accepts only 1 at them moment.`
+      );
+    }
+
+    let url;
+    try {
+      url = new URL(positional[0]);
+    } catch (error: unknown) {
+      console.error(
+        `${red}error${reset} (${programName}): "${
+          positional[0]
+        }" is not a valid URL. ${String(error)}`
+      );
+      process.exit(1);
+    }
+
+    try {
+      const response = await fetch(url.href);
+      const body = await checkStatus(response).text();
+
+      const characters = getUniqueCharacters(body);
+
+      console.log(
+        `${green}success${reset} (${programName}): You url is '${
+          url.href
+        }', and the unique characters are "${characters
+          .map((character) => `'${character}'`)
+          .join(", ")}".`
+      );
+
+      process.exitCode = 0;
+    } catch (error: unknown) {
+      console.error(`${red}error${reset} (${programName}): ${String(error)}`);
+      process.exit(1);
+    }
   }
 
-  if (positional.length > 1) {
-    console.warn(
-      `${yellow}warning${reset} (${programName}): You provided ${positional.length} URLs, but the program accepts only 1 at them moment.`
-    );
-  }
-
-  let url;
-  try {
-    url = new URL(positional[0]);
-  } catch (error: unknown) {
-    console.error(
-      `${red}error${reset} (${programName}): "${
-        positional[0]
-      }" is not a valid URL. ${String(error)}`
-    );
-    process.exit(1);
-  }
-
-  const urlToString = String(url);
-  const characters = getUniqueCharacters(urlToString);
-  console.log(
-    `${green}success${reset} (${programName}): You url is '${urlToString}', and the unique characters are "${characters
-      .map((character) => `'${character}'`)
-      .join(", ")}".`
-  );
-
-  process.exitCode = 0;
-}
-
-process.on("exit", function onExit() {
-  if (process.exitCode !== undefined && process.exitCode !== 0) {
-    console.warn(
-      `${yellow}warning${reset} (${programName}): Program exited with errors.`
-    );
-  } else if (process.exitCode === 0 && verbose) {
-    console.info(
-      `${darkGray}info (${programName}): Program exited successfully.${reset}`
-    );
-  }
-});
+  process.on("exit", function onExit() {
+    if (process.exitCode !== undefined && process.exitCode !== 0) {
+      console.warn(
+        `${yellow}warning${reset} (${programName}): Program exited with errors.`
+      );
+    } else if (process.exitCode === 0 && verbose) {
+      console.info(
+        `${darkGray}info (${programName}): Program exited successfully.${reset}`
+      );
+    }
+  });
+})();
