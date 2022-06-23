a6b0e1fb09f97f45ab3bcf47889dd41ca5bc7481
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2022-06-23 18:05:11 +0300

    Bump version.

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ import { load as cheerioLoad } from "cheerio";
 
 import { darkGray, green, red, reset, yellow } from "./colors.js";
 
-const version = "0.2.1";
+const version = "0.3.0";
 const programName = "scrape";
 const repositoryName = "scrape-url-node";
 
